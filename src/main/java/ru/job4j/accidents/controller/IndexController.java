eab22b5eec57c68cdada0d24a6690db42133270e
alnesterenko <alzpupkin@gmail.com>
2026-05-06 12:45:29 +0300

    0. Spring MVC [#6877]

---
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -0,0 +1,16 @@
+package ru.job4j.accidents.controller;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@ThreadSafe
+@Controller
+public class IndexController {
+    @GetMapping({"/", "/index"})
+    public String getIndex(Model model) {
+        model.addAttribute("pageTitle", "Начальная страница");
+        return "index";
+    }
+}
